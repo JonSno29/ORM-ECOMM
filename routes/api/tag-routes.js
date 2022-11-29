3dae799a2d484abe3ff7ecccadc7d8fd2bd80c29
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-11-29 16:06:06 -0700

    updated tag-routes.js to router.post

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,17 +1,54 @@
 const router = require('express').Router();
+const { async } = require('seed/lib/seed');
 const { Tag, Product, ProductTag } = require('../../models');
+
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-});
+router.get('/', async (req, res) => {
+  try {
+    const tagMD = await Tag.findAll({
+      include: [
+      {
+        model: Product,
+        through: ProductTag,
+        attributes: ['id','product_name','price', 'stock', 'category_id'],
+      },
+    ],
+    });
+    res.status(200).json(tagMD);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+     });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id',],
+      }
+    ]
+})
+.then(dbTagData => {
+  if (!dbTagData) {
+    res.status(404).json({ message: 'No post found with this id'});
+    return;
+  }
+  res.json(dbTagData);
+})
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
 });
+});
+
+  
 
 router.post('/', (req, res) => {
   // create a new tag
