5440ddeefd0dcd5e8c3e5d60f6a2823ca52cd792
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-11-30 16:42:20 -0700

    corrected errors in index and route files

--- models/index.js
+++ models/index.js
@@ -16,8 +16,8 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id',
-  onDelete: 'CASCADE'
+  foreignKey: 'product_id',
+  
 })
 
 // Tags belongToMany Products (through ProductTag)
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,12 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   try {
     const categoryData = await Category.findAll({
       include: [{
         model: Product,
-        as: 'products',}]
+        as: 'products'}],
     });
     res.status(200).json(categoryData);
   } catch (err) {
